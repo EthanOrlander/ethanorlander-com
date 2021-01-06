f412ce1b8be02843d165a52762c5d0b249911dd0
Ethan Orlander <ethanorlander@gmail.com>
2021-01-06 13:33:19 -0500

    Add placeholder homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql, PageProps } from "gatsby";
+import "./styles.css";
 
 interface IndexPageProps extends PageProps {
   data: {
@@ -24,12 +25,42 @@ export const pageQuery = graphql`
 const IndexPage: React.FC<IndexPageProps> = (props) => {
   const { siteName } = props.data.site.siteMetadata;
   return (
-    <>
-      <h1>Heyo TypeScript world!</h1>
+    <div id="main-container">
+      <h1>Welcome to my site.</h1>
       <p>
-        This site is named <strong>{siteName}</strong>
+        I haven't had a chance to set it up yet. In the meantime check out these
+        great organizations:
       </p>
-    </>
+      <a href="https://blacktechpipeline.com/" target="_blank">
+        <h3>Black Tech Pipeline</h3>
+        <p>
+          Bringing exposure, resources and opportunities to Black technologists
+        </p>
+      </a>
+      <a href="https://girlsintech.org/" target="_blank">
+        <h3>Girls in Tech</h3>
+        <p>
+          Inspiring programs and a diverse community to help girls enter into,
+          and excel in, tech
+        </p>
+      </a>
+      <a href="https://blackandbrownfounders.com/" target="_blank">
+        <h3>Black & Brown Founders</h3>
+        <p>
+          We provide community, education, and access to Black and Latinx
+          entrepreneurs, allowing them to launch and build tech businesses with
+          modest resources.
+        </p>
+      </a>
+      <a href="https://femalefoundersfund.com/" target="_blank">
+        <h3>Female Founders Fund</h3>
+        <p>Investing in the exponential power of exceptional female talent.</p>
+      </a>
+      <a href="https://osmihelp.org/" target="_blank">
+        <h3>Open Sourcing Mental Illness</h3>
+        <p>Changing how we talk about mental health in the tech community</p>
+      </a>
+    </div>
   );
 };
 
